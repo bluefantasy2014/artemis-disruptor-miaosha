33f35c58e96bfb63f54c2668e831d025f171de28
shijijun <shijijun@glzc.com>
2018-02-09 16:10:55 +0800

    增加log配置文件

--- jms-server/src/main/java/me/chanjar/jms/server/request/RequestDtoEventDbOutputer.java
+++ jms-server/src/main/java/me/chanjar/jms/server/request/RequestDtoEventDbOutputer.java
@@ -4,6 +4,8 @@ import com.lmax.disruptor.EventHandler;
 import me.chanjar.jms.server.command.infras.Command;
 import me.chanjar.jms.server.command.infras.CommandDispatcher;
 import org.apache.commons.collections.CollectionUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
@@ -11,12 +13,14 @@ import java.util.List;
  * 将结果RequestDtoEvent的结果输出到数据库
  */
 public class RequestDtoEventDbOutputer implements EventHandler<RequestDtoEvent> {
+  private static final Logger LOGGER = LoggerFactory.getLogger(RequestDtoEventDbOutputer.class);
 
 
   private CommandDispatcher commandDispatcher;
 
   @Override
   public void onEvent(RequestDtoEvent event, long sequence, boolean endOfBatch) throws Exception {
+    LOGGER.debug("#3 --- RequestDtoEventDbOutputer called");
 
     if (event.hasErrorOrException()) {
       return;
--- jms-server/src/main/java/me/chanjar/jms/server/request/RequestDtoEventGcHandler.java
+++ jms-server/src/main/java/me/chanjar/jms/server/request/RequestDtoEventGcHandler.java
@@ -1,15 +1,21 @@
 package me.chanjar.jms.server.request;
 
 import com.lmax.disruptor.EventHandler;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import me.chanjar.jms.server.request.RequestDtoEvent;
 
 /**
  * RequestDtoEvent的GC处理器
  */
 public class RequestDtoEventGcHandler implements EventHandler<RequestDtoEvent> {
+  private static final Logger LOGGER = LoggerFactory.getLogger(RequestDtoEventDbOutputer.class);
 
   @Override
   public void onEvent(RequestDtoEvent event, long sequence, boolean endOfBatch) throws Exception {
+    LOGGER.debug("#4 --- RequestDtoEventGcHandler called");
 
     event.clearForGc();
 
